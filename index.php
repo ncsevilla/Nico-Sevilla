492be9f8b2832c4826ff6a842d2fd51a9a3332ff
Nico Sevilla <ncsevilla@hotmail.com>
2011-11-11 01:04:22 +0800

    initial files upload

---
+++ index.php
@@ -0,0 +1,104 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>Nico Sevilla &mdash; Web Developer</title>
+  <meta name="description" content="The personal website of Nico Sevilla. A website developer located in Lucena City, Philippines.">
+  <meta name="author" content="Nico Sevilla">
+  <meta http-equiv="imagetoolbar" content="false" />
+  <!-- Mobile viewport optimized -->
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <!-- Favicon,Humans,Dns -->
+  <link rel="shortcut icon" type="image/x-icon" href="favicon.png" />
+  <link rel="apple-touch-icon" href="apple-touch-icon.png">
+  <link rel="author" href="humans.txt" />
+  <link rel="dns-prefetch" href="//fonts.googleapis.com" />
+  <link rel="dns-prefetch" href="http://www.google.com" />
+  <!-- CSS: implied media=all -->
+  <!-- CSS concatenated and minified via ant build script-->
+  <link rel="stylesheet" type="text/css" media="all" href="css/style.css?v=2" />
+  <link rel="stylesheet" type="text/css" media="all" href="css/960_24_col.css" />
+  <link rel='stylesheet' type='text/css' media="all" href='http://fonts.googleapis.com/css?family=Quattrocento+Sans|Quattrocento&v2' />
+  <link rel='stylesheet' type='text/css' media="all" href='http://fonts.googleapis.com/css?family=Lobster' />
+  <!-- end CSS-->
+
+  <!-- All JavaScript at the bottom, except for Modernizr / Respond.
+       Modernizr enables HTML5 elements & feature detects; Respond is a polyfill for min/max-width CSS3 Media Queries -->
+  <script src="js/libs/modernizr-2.0.6.min.js"></script>
+</head>
+
+<body id="page_effect" style="display:none;">
+
+  <div id="container" >
+    <div id="main" role="main">
+  
+    	<div id="nav" class="shadow-2">  
+        	<div class="container_24">
+            	<div class="grid_24">               
+                	<p id="text">Nico Sevilla</p>                                            
+                </div> <!-- end .grid_24 -->
+            </div> <!-- end .container_24 --> 
+        </div> <!-- end #nav--> 
+    
+    	<div id="content"> 
+			<div class="container_24">
+        		<div id="wrapper" class="shadow radius-bl radius-br" >
+ 					<div class="grid_24">
+              			<div class="nico">Nico Sevilla</div>
+                        <div class="rectangle"></div>
+                        <div class="triangle-l"></div>
+						<div class="triangle-r"></div>
+              			<p>This new website is currently under development. You can contact me at my email ncsevilla@hotmail.com. Thank You!</p 
+               		 </div> <!-- end .grid_24 -->
+           	    </div> <!-- end #wrapper -->         
+       		</div> <!-- end .container_24 -->
+        </div> <!-- end #content --> 
+
+       <div id="footer">  
+        	<div class="container_24">
+            	<div class="grid_24">
+                	<span id="footer-text"><?php require_once('php/scripts/copyrights.php'); ?></span>  
+                </div> <!-- end .grid_24 -->
+            </div> <!-- end .container_24 --> 
+        </div> <!-- end #footer -->        
+        
+    </div> <!--! end of #main -->
+  </div> <!--! end of #container -->
+
+
+  <!-- JavaScript at the bottom for fast page loading -->
+
+  <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/libs/jquery-1.6.2.min.js"><\/script>')</script>
+
+
+  <!-- scripts concatenated and minified via ant build script-->
+  <script defer src="js/plugins.js"></script>
+  <script defer src="js/script.js"></script>
+  <!-- end scripts-->
+
+	
+  <!-- Google Analytics -->
+  <script>
+    window._gaq = [['_setAccount','UA-26852638-1'],['_trackPageview'],['_trackPageLoadTime']];
+    Modernizr.load({
+      load: ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js'
+    });
+  </script>
+  
+  
+  <!-- Prompt IE 6 users to install Chrome Frame. Remove this if you want to support IE 6.
+       chromium.org/developers/how-tos/chrome-frame-getting-started -->
+  <!--[if lt IE 7 ]>
+    <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+    <script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
+  <![endif]-->
+  
+
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,82 @@
+/* Author: Nico Sevilla
+
+*/
+
+
+  <!-- jQuery fade in page load -->  
+  $(document).ready(function(){
+  $('#page_effect').fadeIn(2000);
+  });<!-- end jQuery fade in page load --> 
+
+  <!-- jQuery animate #nav opacity -->
+  $(function() {
+
+	  $(window).scroll(function(){
+
+		  var scrollTop = $(window).scrollTop();
+
+		  if(scrollTop != 0)
+
+			  $('#nav').stop().animate({'opacity':'0.2'},400);
+
+		  else	
+
+			  $('#nav').stop().animate({'opacity':'1'},400);
+
+	  });
+
+	  
+
+	  $('#nav').hover(
+
+		  function (e) {
+
+			  var scrollTop = $(window).scrollTop();
+
+			  if(scrollTop != 0){
+
+				  $('#nav').stop().animate({'opacity':'1'},400);
+
+			  }
+
+		  },
+
+		  function (e) {
+
+			  var scrollTop = $(window).scrollTop();
+
+			  if(scrollTop != 0){
+
+				  $('#nav').stop().animate({'opacity':'0.2'},400);
+
+			  }
+
+		  }
+
+	  );
+
+  }); <!-- end jQuery animate #nav opacity -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
